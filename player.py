f18eb5f193ecf46fd1b96ddf8a3867f607c68c57
Asakura <Asakura-MBA>
2025-04-06 17:00:47 +0200

    added reset option

--- player.py
+++ player.py
@@ -5,17 +5,17 @@ MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
+class Player(Turtle):
     def __init__(self):
-        self.player = Turtle()
-        self.player.shape("turtle")
-        self.player.color("Black")
-        self.player.penup()
-        self.player.goto(STARTING_POSITION)
-        self.player.seth(90)
+        super().__init__()
+        self.shape("turtle")
+        self.color("Black")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.seth(90)
     
     def up(self):
-        self.player.forward(MOVE_DISTANCE)
-
-    def down(self):
-        self.player.back(MOVE_DISTANCE)
+        self.forward(MOVE_DISTANCE)
+        
+    def finish(self):
+        self.goto(STARTING_POSITION)
