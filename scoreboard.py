b49ba21b627889faaa8387e1a93ba436d8563469
Asakura <Asakura-MBA>
2025-04-06 17:01:10 +0200

    added level_up function and show current level

--- scoreboard.py
+++ scoreboard.py
@@ -8,9 +8,14 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.color("black")
         self.penup()
-        self.goto(-220,260)
         self.score = 1
         self.level()
         
     def level(self):
+        self.goto(-220,260)
         self.write(f"Level: {self.score}", True, "center", font=FONT)
+        
+    def level_up(self):
+        self.score += 1
+        self.clear()
+        self.level()
